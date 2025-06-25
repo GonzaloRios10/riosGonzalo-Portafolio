e43df81b7307cb9ac25bf52f346ff3e197a4920d
GonzaloRios10 <jugador.gonzarios@gmail.com>
2025-06-25 16:12:59 -0300

    Agrego gh-pages como dependencia de desarrollo para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/riosGonzalo-Portafolio/',
   plugins: [react()],
   css: {
     modules: {
       localsConvention: "camelCase",
     },
   },
-});
+})
